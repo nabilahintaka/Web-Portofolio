9bea9eecdc7bfb7ed8e337531581d6ae52fb41c0
nabilahintaka <nabilahintaka@gmail.com>
2024-12-25 22:21:08 +0700

    add tools

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -25,9 +25,7 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGooglebigquery />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <SiTableau />
       </Col>
