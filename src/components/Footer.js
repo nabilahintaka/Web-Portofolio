ae13784f92d99c566d4f52255e8785833e351a49
nabilahintaka <nabilahintaka@gmail.com>
2025-01-24 01:00:42 +0700

    update discord

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,16 +23,16 @@ function Footer() {
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
 
-            <li className="social-icons">
+            {/* <li className="social-icons">
               <a
-                href="https://discord.gg/BuKXD5MM"
+                href="discord.gg/BuKXD5MM"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
               >
                 <AiOutlineDiscord />
               </a>
-            </li>
+            </li> */}
             
             {/* <li className="social-icons">
               <a
